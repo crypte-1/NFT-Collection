13c34458dcd5a3b31393c5e8ff0da7664e8f5b5c
crypte-1 <renukavutukuri1@gmail.com>
2022-06-01 20:06:47 +0530

    Added constants

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x031eDA8a282695589A5d72ec3fc23b51939fafc6";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
